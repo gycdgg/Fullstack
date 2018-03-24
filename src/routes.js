44f7e53cdb4d0e292c0273bb1f7c715f0012f2e9
gycdgg <1165297373@qq.com>
2018-03-24 19:45:37 +0800

    add prop-types module

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
-/**
- * Created by kadven on 2017/1/23.
- */
-import React from 'react';
-import {Route,IndexRoute} from 'react-router';
-import { Wrapper,Index } from './components';
-import {NotFound,ServerError,GatewayTimeout} from './components/error_page';
+import React from 'react'
+import { Route,IndexRoute } from 'react-router'
+import { Wrapper,Index } from './components'
+import  LazyLoadImage  from './pages/basic/LazyloadImage'
+import { NotFound } from './components/error_page'
 let childRoutes =
-    <Route path="/" component={ Wrapper }>
-        <IndexRoute component={Index}/>
+<Route path="/" component={ Wrapper }>
+        <IndexRoute component={ Index }/>
+        <Route path = "/basic/image" component = { LazyLoadImage }/>
+        <Route path="*" component={NotFound} />
     </Route>
-export default (childRoutes);
+export default (childRoutes)
