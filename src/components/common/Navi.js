e0c4070ece948ceeeaf4194ce9b46de166990cfc
gycdgg <1165297373@qq.com>
2018-05-21 23:54:49 +0800

    modified footer component

--- src/components/common/Navi.js
+++ src/components/common/Navi.js
@@ -1,41 +1,58 @@
-import React from "react";
-import styles from "./styles.styl"
-class Navi extends React.Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            style:{},
-            show: false
-        }
-        this.data = ['aaaaaaaaaaaa','bbbbbbbbbb','cccccccccccc','aaaaaaaaaaaa','bbbbbbbbbb','cccccccccccc','aaaaaaaaaaaa','bbbbbbbbbb','cccccccccccc']
+import React from 'react'
+import styles from './styles.styl'
+class Navi extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      style: {},
+      show: false
     }
+    this.data = [
+      'aaaaaaaaaaaa',
+      'bbbbbbbbbb',
+      'cccccccccccc',
+      'aaaaaaaaaaaa',
+      'bbbbbbbbbb',
+      'cccccccccccc',
+      'aaaaaaaaaaaa',
+      'bbbbbbbbbb',
+      'cccccccccccc'
+    ]
+  }
 
-    handleEnter = (e) => {
-        e.stopPropagation()
-        this.setState({
-            style:{backgroundColor:"red"},
-            show: true
-        })
-    }
+  handleEnter = (e) => {
+    e.stopPropagation()
+    this.setState({
+      style: {
+        backgroundColor: 'red'
+      },
+      show: true
+    })
+  }
 
-    handleLeave = (e) => {
-        e.stopPropagation()
-        this.setState({
-            style:{},
-            show: false
-        })
-    }
-    render(){
-        const { content } =  this.props
-        return <div className = {styles.navi} onMouseEnter = { this.handleEnter } onMouseLeave ={ this.handleLeave }>
-            <div className = {styles.navi__content} style = {this.state.style}>{content}</div>
-            { this.state.show ? <div className = {styles.navi__hidden}>
-                { this.data.map((v,i)=>{
-                    return <div>{v}</div>
-                })}
-            </div> : null}
-        </div>
-    }
+  handleLeave = (e) => {
+    e.stopPropagation()
+    this.setState({style: {}, show: false})
+  }
+  render() {
+    const {content} = this.props
+    return <div
+      className={styles.navi}
+      onMouseEnter={this.handleEnter}
+      onMouseLeave
+      ={this.handleLeave}>
+      <div className={styles.navi__content} style={this.state.style}>{content}</div>
+      {this.state.show
+        ? <div className={styles.navi__hidden}>
+            {this
+              .data
+              .map((v, i) => {
+                return <div key={i}>{v}</div>
+              })}
+          </div>
+        : null}
+    </div>
+  }
 }
 
 export default Navi
--- src/components/wrapper/footer.js
+++ src/components/wrapper/footer.js
@@ -5,7 +5,7 @@ import styles from './styles.styl'
 
 
 const config = {
-    type:["products","Markets","Support","About Gigalight"],
+    type:["Products","Support","AboutGigalight"],
     Products:["xWDM & OADM","MTP/MPO Fiber Cables","Optical Transceivers","Active Optical Cables","Fiber Patch Cables","Passive Components"],
     Solutions:["FTTX Solution","FTTX Solution"],
     Support:["Service","RMA","Download Center","Knowledge Center"],
@@ -13,7 +13,7 @@ const config = {
 }
 class Footer extends Component {
     constructor(props) {
-        super(props);
+      super(props)
     }
 
     render() {
@@ -21,22 +21,18 @@ class Footer extends Component {
             <footer className={styles.footer}>
                 <div className = {styles.footer__content}>
                     <div className = {styles.footer__content__banner}>
-                        <span>
-                            <div className = {styles.footer__content__banner__title}>Products</div>
-                            <ul className = {styles.footer__content__banner__item}>
-                                <li>xWDM & OADM</li>
-                                <li>MTP/MPO Fiber Cables</li>
-                                <li>Optical Transceivers</li>
-                                <li>Active Optical Cables</li>
-                                <li>Direct Attach Cables</li>
-                                <li>Fiber Patch Cables</li>
-                                <li>Passive Components</li>
-                                <li>Optical Amplifiers </li>
-                            </ul>
-                        </span>
-                        <span>Markets</span>
-                        <span>Support</span>
-                        <span>About Gigalight</span>
+                        {
+                            config.type.map((v,i)=>{
+                              return <span key = {i}>
+                                <div className = {styles.footer__content__banner__title}>{v}</div>
+                                <ul className = {styles.footer__content__banner__item}>
+                                  {config[v].map((_v,_i)=>{
+                                     return <li key = {_i}>{_v}</li>
+                                  })}
+                                </ul>
+                          </span>
+                            })
+                        }
                     </div>
                     All rights reserved @ Edmond Guan
                 </div>
