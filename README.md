1fa3dfa65c52871685b086f4333d84f81d7c97a1
gycdgg <1165297373@qq.com>
2018-05-25 17:53:07 +0800

    fix css and format code

--- README.md
+++ README.md
@@ -1,16 +1,9 @@
 ## Full Stack demo by React and Nodejs
 
-写这个的初衷也是为了交流学习，自己的可以自由发挥。
-
-初步构想的是
-> **FrontEnd: React + Redux + Antd + style + webpack**
+> **FrontEnd: React + Redux + Antd + styles + webpack**
 
 > **BackEnd: Node + mysql + redis + sequelize + jwt**
 
-以后可能会用TS重构
-
-里面会写一些基础组件，也会自己实现一些控件。
-欢迎Fork，能Star一下最好啦
 
 #### 项目运行
 DEV 环境
@@ -18,7 +11,7 @@ DEV 环境
 2. npm run dll
 3. npm run dev
 
-也可以build
+
 npm run build
 
 
--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,33 +1,43 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Menu, Icon, Breadcrumb } from 'antd'
 import { withRouter,Link } from 'react-router'
+import styles from './styles.styl'
 const SubMenu = Menu.SubMenu
 const MenuItemGroup = Menu.ItemGroup
-import styles from './styles.styl'
+
 class MenuList extends React.Component{
+
   constructor(props){
     super(props)
   }
+
   handleClick = (e) => {
     console.log('click ', e);
   }
+
   itemRender = (route,params,routes,paths) => {
     const last = routes.indexOf(route) === routes.length - 1;
     return last ? <span>{route.breadcrumbName}</span> : <Link to={paths.join('/')}>{route.breadcrumbName}</Link>;
   }
 
+  /**
+   * @argument: navi is a array
+   * map navi and render it logically
+   */
   naviRender = (navi) => {
     return navi.map((v,i) => {
       if(Array.isArray(v)){
-        return <SubMenu key = {v} title = {v.shift(1)}>
+        return <SubMenu key = {v+i} title = {v.shift(1)}>
           {this.naviRender(v)}
         </SubMenu>
       } else {
-        return <Menu.Item>{v}</Menu.Item>
+        return <Menu.Item key = {v+i} className = {styles.item}>{v}</Menu.Item>
       }
     }
     )
   }
+
   render(){
     // get props from father component
     const { routes,title,navi } = this.props
@@ -44,18 +54,23 @@ class MenuList extends React.Component{
             onClick={this.handleClick}
             defaultSelectedKeys={['5']}
             defaultOpenKeys={['sub1']}
-            mode="inline"
-          >
-            {this.naviRender(navi)}
+            mode="inline">
+            { this.naviRender(navi) }
           </Menu>
-          {/* {this.props.navi} */}
         </div>
         <div className = {styles.menu__container__content}>
-          {this.props.children}
+          { this.props.children }
         </div>
       </div>
     </div>
   }
+}
+
+MenuList.PropTypes = {
+  props: PropTypes.object.isRequired,
+  title: PropTypes.string.isRequired,
+  children: PropTypes.element.isRequired,
+  navi: PropTypes.array.isRequired
 }
 
 export default MenuList
