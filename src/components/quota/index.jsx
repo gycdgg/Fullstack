223b2fd4a3afeed14b33de73cc501dcd488d6d10
gycdgg <1165297373@qq.com>
2018-06-13 17:24:57 +0800

    add quota pag

--- src/components/quota/index.jsx
+++ src/components/quota/index.jsx
@@ -1,12 +1,136 @@
 import React from 'react'
-
+import styles from './styles.styl'
+import { Form, Select, Input, Button, Col, Row } from 'antd'
+const FormItem = Form.Item
+const Option = Select.Option
+@Form.create()
 class Quota extends React.Component{
   constructor(props){
     super(props)
   }
 
   render(){
-    return <div>11111</div>
+    const { getFieldDecorator } = this.props.form
+    return <div className = {styles.quota}>
+      <header className = {styles.quota__header}>Get a Quick Quota</header>
+      <p className = {styles.quota__text}>You can contact Ingellen Live Chat at the top-right corner to get real-time answers to your questions, or email Ingellen Technical Support where most issues are resolved within 24 hours.</p>
+      <div className = {styles.quota__forms}>
+        <Form onSubmit={this.handleSubmit}>
+          <Row  gutter={12}>
+            <Col span={6}>
+              <FormItem
+              colon = {false}
+              label="First Name"
+              >
+              {getFieldDecorator('FirstName', {
+                rules: [{ required: true, message: 'Please input your first name!' }],
+              })(
+                <Input />
+              )}
+              </FormItem>
+            </Col>
+            <Col span={6}>
+              <FormItem
+              colon = {false}
+              label="Last Name"
+              >
+              {getFieldDecorator('gender', {
+                rules: [{ required: true, message: 'Please select your last name!' }],
+              })(
+                <Select
+                  placeholder="Select a option and change input text above"
+                  onChange={this.handleSelectChange}
+                >
+                  <Option value="male">male</Option>
+                  <Option value="female">female</Option>
+                </Select>
+              )}
+            </FormItem>
+            </Col>
+          </Row>
+          <Row  gutter={12}>
+          <Col span={6}>
+            <FormItem
+            colon = {false}
+            label="Email"
+            >
+            {getFieldDecorator('email', {
+              rules: [{ required: true, message: 'Please input your email!' }],
+            })(
+              <Input />
+            )}
+            </FormItem>
+          </Col>
+          <Col span={6}>
+            <FormItem
+            colon = {false}
+            label="Company"
+            >
+            {getFieldDecorator('company')(
+              <Select
+                placeholder="Select a option and change input text above"
+                onChange={this.handleSelectChange}
+              >
+                <Option value="male">male</Option>
+                <Option value="female">female</Option>
+              </Select>
+            )}
+          </FormItem>
+          </Col>
+        </Row>
+        <Row  gutter={12}>
+        <Col span={6}>
+          <FormItem
+          colon = {false}
+          label="Country"
+          >
+          {getFieldDecorator('country', {
+            rules: [{ required: true, message: 'Please input your country!' }],
+          })(
+            <Input />
+          )}
+          </FormItem>
+        </Col>
+        <Col span={6}>
+          <FormItem
+          colon = {false}
+          label="Phone Number"
+          >
+          {getFieldDecorator('phone')(
+            <Select
+              placeholder="Select a option and change input text above"
+              onChange={this.handleSelectChange}
+            >
+              <Option value="male">male</Option>
+              <Option value="female">female</Option>
+            </Select>
+          )}
+        </FormItem>
+        </Col>
+      </Row>
+      <Row gutter={12}>
+        <Col span = {12}>
+          <FormItem
+            colon = {false}
+            label="share the product details with us"
+            >
+            {getFieldDecorator('detail', {
+              rules: [{ required: true, message: 'Please select your phone number!' }],
+            })(
+              <Input.TextArea placeholder = "Tell us your purpose for the same application or share your describtions, quantities to the items that you need to apply for a sample if no product ID online"/>
+            )}
+          </FormItem>
+        </Col>
+      </Row>
+          <FormItem
+          >
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </FormItem>
+      </Form>
+      </div>
+    </div>
   }
 }
 
