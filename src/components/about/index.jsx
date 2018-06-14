6ce95de648c7ae451b18d106755dcb6723e404c4
gycdgg <1165297373@qq.com>
2018-06-14 14:54:33 +0800

    add about me page

---
+++ src/components/about/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class About extends React.Component{
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    <div>about us page</div>
+  }
+}
+
+export default About
