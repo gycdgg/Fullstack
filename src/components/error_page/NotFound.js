84198940efaa0e74cbc6b7b9cdf2cd1ee333a31d
gycdgg <1165297373@qq.com>
2018-06-23 02:47:46 +0800

    test

--- src/components/error_page/NotFound.js
+++ src/components/error_page/NotFound.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
-import {Link} from 'react-router';
-import Error from './Error';
-import {notfound} from '../../assets/img';
+import React, { Component } from 'react'
+import { Link } from 'react-router'
+import Error from './Error'
+import { notfound } from '../../assets/img'
 class NotFound extends Component {
-    constructor(props) {
-        super(props)
-    }
+  constructor(props) {
+    super(props)
+  }
 
-    render() {
-        return <Error
+  render() {
+    return <Error
             imgUrl={notfound}
             altText="not found"
-            errorMsg="很抱歉，您访问的页面不在地球上..."
+            errorMsg="很抱歉，您访问的页面不在地球上.../Users/mac/Desktop/code/study/fullstack/dist"
             btnText="返回首页"
         />
-    }
+  }
 }
 export default NotFound
