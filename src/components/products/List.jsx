308a1efbc09ecef77d93843e5334d27a03aa7c7b
gycdgg <1165297373@qq.com>
2018-07-18 17:47:12 +0800

    product list page

--- src/components/products/List.jsx
+++ src/components/products/List.jsx
@@ -1,43 +1,75 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
 import styles from './styles'
 import { Divider, Pagination, Input } from 'antd'
 import Item from './Item'
 import fetch from '$fetch'
-/**
- * use mock data for product item
- */
-const products = new Array(10).fill('this is test product')
+
+@connect(({ product }) => ({ product }), (dispatch, ownprops) => ({
+  changePage: (...args) => dispatch(actions.changePage(...args)),
+  changeCategory: (...args) => dispatch(actions.changeCategory(...args))
+}))
+
 class List extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      productList: []
-    }
-  }
-  componentDidMount() {
-    fetch('/api/client/products').then(res => {
-      this.setState({
-        productList: res.rows
-      })
-    })
+
+  static propTypes = {
+    location: PropTypes.object.isRequired
   }
 
-  getProductList = () => {
-    // const offset = 
-    fetch('/api/client/products').then()
+  constructor(props) {
+    super(props)
+    this.limit = 10
+    console.log('this.props.location', this.props.location)
+    this.state = {
+      amount: 0,
+      category: this.props.location.query.category,
+      productList: [],
+      page: +this.props.location.query.page || 1
+    }
   }
+
+  componentDidMount() {
+    this.getProductList()
+  }
+
+  onPageSizeChange = (e) => {
+    console.log(e)
+  }
+  /**
+   * transfer limit offset and category to backend
+   */
+  getProductList = () => {
+    let { page, category } = this.state
+    let offset = (page - 1) * (this.limit)
+    fetch(`/api/client/products?limit=${this.limit}&offset=${offset}&category=${category}`).then((res) =>
+      this.setState({
+        amount: res.count,
+        productList: res.rows
+      })
+    )
+  }
+
   render() {
+    console.log('this.props', this.props)
+    const { page: current,  amount: total } = this.state
     return <div className = { styles.list }>
       <div className = { styles.list__header }>list header</div>
       <Divider className = { styles.divider }/>
       <div className = { styles.list__search }>
-        <Input onChange = { (e) => this.handleInputChange(e) }/>
+        <Input onChange = { (e) => this.handleInputChange(e) } placeholder = "input product name which you want to search"/>
       </div>
       <Divider dashed className = { styles.divider }/>
       <div className = { styles.list__content }>
         { this.state.productList.map((v, i) => <Item title = { v.name } content = { v.summary } key = { i }/>) }
       </div>
-      <Pagination showQuickJumper defaultCurrent = { 2 } total = { 500 } onChange = { this.onChange }
+      <Pagination 
+          showQuickJumper 
+          defaultCurrent = { 1 } 
+          current = { current }
+          pageSize = { 10 }
+          total = { total }
+          onChange = { this.onPageSizeChange }
           className = { styles.list__page }
       />
     </div>
